863f92593fe0fdf59ec0a7930dc710e05a3b178f
liaoyf <893753543@qq.com>
2018-03-12 18:06:12 +0800

    init

--- index.js
+++ index.js
@@ -1,8 +1,12 @@
-const webfontsGenerator = require('webfonts-generator');
+require("colors");
+const webfontsGenerator = require('@liaoyf/webfonts-generator');
 const rimraf = require('rimraf');
 const fs = require('fs');
-const handlebars = require('handlebars')
-require("colors");
+const handlebars = require('handlebars');
+const _ = require("lodash");
+
+const distPath = 'dist/';
+const srcPath = 'src/';
 
 function templateHelpers() {
   handlebars.registerHelper('removePeriods', function (selector) {
@@ -13,6 +17,9 @@ function templateHelpers() {
   });
   handlebars.registerHelper('removeCh', function(string){
     return string.replace(/[\u4e00-\u9fa5]+_/gi,"");
+  });
+  handlebars.registerHelper('getCodepoint', function(name, codepoints){
+    return '\\' + (codepoints[name] || '');
   });
 }
 
@@ -29,6 +36,21 @@ function getSvgFiles(parentPath) {
   return files.map(file => parentPath + file);
 }
 
+function getTypes(files){
+  const result = _.groupBy(files, (value, index, collection) => {
+    return value.split('_')[0];
+  });
+  const group = [];
+  for(let prop in result){
+    group.push({
+      type: prop,
+      list: result[prop]
+    });
+  }
+
+  return group;
+}
+
 function generatorFont(files) {
   return new Promise((resolve, reject) => {
     webfontsGenerator({
@@ -38,9 +60,11 @@ function generatorFont(files) {
       html: true,
       templateOptions: {
         classPrefix: 'si-',
-        baseSelector: '.si'
+        baseSelector: '.si',
+        types: getTypes(fs.readdirSync(srcPath).map(file => file.split('.')[0]))
       },
-      htmlTemplate: "templates/html.hbs"
+      htmlTemplate: "templates/html.hbs",
+      cssTemplate: "templates/css.hbs"
     }, function (error) {
       if (error) {
         reject(error);
@@ -53,10 +77,10 @@ function generatorFont(files) {
 
 
 templateHelpers();
-removeDist("dist/")
+removeDist(distPath)
   .then(() => {
     console.log("√ 清除dist文件夹!".green);
-    generatorFont(getSvgFiles("src/"))
+    generatorFont(getSvgFiles(srcPath))
       .then(() => {
         console.log(`√ 成功生成字体图标！`.green);
       })
