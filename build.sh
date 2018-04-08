7a2aa4ac9a572e05500c56296dbe4041c281e35a
liaoyf <893753543@qq.com>
2018-04-08 10:26:25 +0800

    [修改][@share/shareui-font]测试bat命令

--- build.sh
+++ build.sh
@@ -1,10 +1,19 @@
 #!/bin/bash
 
+PACKAGE_VERSION=$(cat package.json \
+  | grep version \
+  | head -1 \
+  | awk -F: '{ print $2 }' \
+  | sed 's/[",]//g')
+
 echo 请填写提交日志：
 read log
-git checkout dev
 git add -A
-git commit -am $log
-git push
+git commit -m $log
+git push origin master
+git tag -a $PACKAGE_VERSION -m $log
+git push --tags
 echo Press Enter...
 read
+#echo "Done"
+#sleep 6
